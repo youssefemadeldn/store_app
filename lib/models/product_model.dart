4d97738bcf903f1e14baf1e63f8fce0477286bb1
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-11-12 16:57:58 +0200

    create ProductModel: product and rating model 1

---
+++ lib/models/product_model.dart
@@ -0,0 +1,42 @@
+class ProductModel {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+  final RatingModel rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductModel.fromJson(jsonData) {
+    return ProductModel(
+      id: jsonData['id'],
+      title: jsonData['title'],
+      price: jsonData['price'],
+      description: jsonData['description'],
+      category: jsonData['category'],
+      image: jsonData['image'],
+      rating: RatingModel.fromJson(jsonData['rating']),
+    );
+  }
+}
+
+class RatingModel {
+  final double rate;
+  final int count;
+
+  RatingModel({required this.rate, required this.count});
+
+  factory RatingModel.fromJson(jsonData) {
+    return RatingModel(rate: jsonData['rate'], count: jsonData['count']);
+  }
+}
