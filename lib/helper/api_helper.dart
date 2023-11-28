580f998a97bfd191a1af852734e35f70642a33ef
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-11-28 20:04:25 +0200

    create post method in ApiHelper: post method 1

--- lib/helper/api_helper.dart
+++ lib/helper/api_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class ApiHelper {
@@ -8,6 +9,25 @@ class ApiHelper {
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
+    } else {
+      throw Exception(
+          'there is a problem with status code ${response.statusCode}');
+    }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+
+    if (response.statusCode == 200) {
+      return (response.body);
     } else {
       throw Exception(
           'there is a problem with status code ${response.statusCode}');
