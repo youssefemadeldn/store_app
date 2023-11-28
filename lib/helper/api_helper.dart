4c7770525527b354b3731fb7799963dbe36dd03b
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-11-28 20:36:39 +0200

    create put method in ApiHelper: put method 1

--- lib/helper/api_helper.dart
+++ lib/helper/api_helper.dart
@@ -33,4 +33,24 @@ class ApiHelper {
           'status code error: ${response.statusCode}, body error: ${jsonDecode(response.body)}');
     }
   }
+
+  Future<dynamic> put(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.put(Uri.parse(url), body: body, headers: headers);
+
+    if (response.statusCode == 200) {
+      return (response.body);
+    } else {
+      throw Exception(
+          'status code error: ${response.statusCode}, body error: ${jsonDecode(response.body)}');
+    }
+  }
 }
