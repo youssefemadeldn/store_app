2e244541fdd956aae50b8435d308efb76b4d9dbc
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-11-29 09:59:01 +0200

    edit on home page: home page ui 2

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
+  static String id = 'HomePage';
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
