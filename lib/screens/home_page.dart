c10e6e314eebf1d249dfef76a5372f1375960da6
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-11-30 07:52:10 +0200

    create CustomCard: grid view 1

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/widgets/custom_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -22,54 +23,10 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
+      body: const Center(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Container(
-                alignment: Alignment.topLeft,
-                height: 90,
-                width: 150,
-                decoration: const BoxDecoration(),
-                child: const Card(
-                  elevation: 10,
-                  child: Padding(
-                    padding: EdgeInsets.all(4.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Handelbag LV',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('\$225'),
-                            Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 30,
-                bottom: 60,
-                child: Image.network(
-                  'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-                  height: 70,
-                ),
-              ),
-            ],
-          ),
+          padding: EdgeInsets.all(8.0),
+          child: CustomCard(),
         ),
       ),
     );
---
+++ lib/widgets/custom_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          height: 90,
+          width: 150,
+          decoration: const BoxDecoration(),
+          child: const Card(
+            elevation: 10,
+            child: Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Handelbag LV',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('\$225'),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 30,
+          bottom: 60,
+          child: Image.network(
+            'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+            height: 70,
+          ),
+        ),
+      ],
+    );
+  }
+}
