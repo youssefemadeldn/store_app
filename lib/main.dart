64eb1cd651feac59c9527652c2d774fdfc116cea
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-12-02 10:11:27 +0200

    create it empty: update product page 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/screens/home_page.dart';
+import 'package:store_app/screens/update_page.dart';
 
 void main() {
   runApp(const StoreApp());
@@ -13,6 +14,7 @@ class StoreApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         HomePage.id: (context) => HomePage(),
+        UpdateProductPage.id: (context) => UpdateProductPage(),
       },
       initialRoute: HomePage.id,
     );
---
+++ lib/screens/update_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UpdateProductPage extends StatelessWidget {
+  static String id = 'UpdateProductPage';
+  const UpdateProductPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
