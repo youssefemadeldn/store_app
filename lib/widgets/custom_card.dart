a8cd74b999158cc69fccb4d7508bd3d9d550fbb6
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-12-02 10:13:31 +0200

    GestureDetector in card: update product page 2

--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/models/product_model.dart';
+import 'package:store_app/screens/update_page.dart';
 
 class CustomCard extends StatelessWidget {
   ProductModel product;
@@ -8,49 +9,54 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          decoration: const BoxDecoration(),
-          child: Card(
-            elevation: 10,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    product.title.substring(0, 6),
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('\$${product.price.toString()}'),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      ),
-                    ],
-                  ),
-                ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, UpdateProductPage.id);
+      },
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            decoration: const BoxDecoration(),
+            child: Card(
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.title.substring(0, 6),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('\$${product.price.toString()}'),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Positioned(
-          right: 30,
-          bottom: 70,
-          child: Image.network(
-            product.image,
-            height: 95,
-            width: 95,
+          Positioned(
+            right: 30,
+            bottom: 70,
+            child: Image.network(
+              product.image,
+              height: 95,
+              width: 95,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
